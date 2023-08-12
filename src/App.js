fed28ba2d3c6b8fab0ae2a599ee4d97e0876abd9
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-08-12 01:28:22 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { lightMode, darkMode } from "./Components/UI/temas";
 
--- src/Components/Container/Container.styled.jsx
+++ src/Components/Container/Container.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.body};
-  min-height: 90vh;
+  min-height: 90.2vh;
   padding: 0px 15vw;
 `;
 
